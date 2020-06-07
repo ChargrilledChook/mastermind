2be763bd2c60cd01711751751b45ba0f20bb27f5
Dylan Williams <happyfrog578+github@gmail.com>
2020-06-07 14:27:19 +1000

    add rubocop changes - change conditional formatting

--- cracker.rb
+++ cracker.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
-require 'pry'
-# not sure if i need this class
+
+# Responsible for cracking the code if player wants to be codemaster
 class Cracker
-  attr_accessor :guess_log, :p_code
+  attr_accessor :p_code
 
   def initialize
-    @guess_log = [[0, 0, 0, 0]]
     @p_code = []
   end
 
@@ -16,13 +15,12 @@ class Cracker
   def crack_code(last_guess)
     current_guess = []
     last_guess.each_with_index do |e, idx|
-      if e == p_code[idx]
-        current_guess[idx] = p_code[idx]
-      else
-        current_guess[idx] = guess_code(1)
-      end
+      current_guess[idx] = if e == p_code[idx]
+                             p_code[idx]
+                           else
+                             guess_code(1)
+                           end
     end
-    guess_log << current_guess
     current_guess.flatten!
   end
 end
