a2c123503f5d3c4446033c70571120c92861aa55
Dylan Williams <happyfrog578+github@gmail.com>
2020-06-07 10:30:38 +1000

    add crack code prototype

--- cracker.rb
+++ cracker.rb
@@ -2,11 +2,27 @@
 
 # not sure if i need this class
 class Cracker
+  attr_accessor :guess_log
+
+  def initialize
+    @guess_log = [[0, 0, 0, 0]]
+  end
+
   def guess_code(num = 4)
     num.times.map { rand(1..6) }
   end
+
+  def crack_code(last_guess)
+    current_guess = []
+    last_guess.each_with_index do |e, idx|
+      if e == 'C'
+        current_guess[idx] = guess_log[-1][idx]
+      else
+        current_guess[idx] = guess_code(1)
+      end
+    end
+    guess_log << current_guess
+    current_guess
+  end
 end
 
-cracker = Cracker.new
-
-print cracker.guess_code
